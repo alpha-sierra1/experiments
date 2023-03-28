742eb1fc3bb8caca14974ab3357bbe5bb809ff8a
AditiGi <aditisingh20144@gmail.com>
2023-03-29 00:08:51 +0530

    still WIP

--- Random-input/script.js
+++ Random-input/script.js
@@ -1,29 +1,33 @@
+// "use strict";
+
+// const dropdownHeading = document.querySelector(".box-1");
+// const dropdownButton = document.querySelector(".box-1 button");
+// const dropdownBox = document.querySelector(".box-2");
+// const dropdownMenuItems = document.querySelectorAll("box-2 p");
+
+// // if (
+// //   dropdownButton.addEventListener(
+// //     "click",
+// //     dropdownMenu.classList.remove("hidden")
+// //   )
+// // );
+
+// dropdownMenuItems.forEach((menuItem) => {
+//   menuItem.addEventListener("click", () => {
+//     dropdownBox.classList.add("hidden");
+//   });
+// });
+
+// dropdownButton.addEventListener("click", () => {
+//   dropdownBox.classList.remove("hidden");
+// });
+
 "use strict";
 
-const dropdownHeading = document.querySelector(".box-1");
 const dropdownButton = document.querySelector(".box-1 button");
 const dropdownBox = document.querySelector(".box-2");
-const dropdownMenuItems = document.querySelectorAll("box-2 p");
+const dropdownItems = document.querySelectorAll("box-2 p");
 
-// if (
-//   dropdownButton.addEventListener(
-//     "click",
-//     dropdownMenu.classList.remove("hidden")
-//   )
-// );
-
-("use strict");
-
-function toggleDropdown() {
-  dropdownBox.classList.toggle("hidden");
-}
-
-function hideDropdown() {
-  dropdownBox.classList.add("hidden");
-}
-
-dropdownButton.addEventListener("click", toggleDropdown);
-
-dropdownMenuItems.forEach((menuItem) => {
-  menuItem.addEventListener("click", hideDropdown);
+dropdownButton.addEventListener("click", () => {
+  dropdownItems.classList.toggle("hidden");
 });
