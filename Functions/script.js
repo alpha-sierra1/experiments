1b924a40f3fbe36bdceb9cd8bace19bc213372de
AditiGi <aditisingh20144@gmail.com>
2023-07-04 17:33:48 +0530

    sdfsvg

--- Functions/script.js
+++ Functions/script.js
@@ -1,3 +1,4 @@
+// Function Declaration
 function isEven(number) {
   if (number % 2) {
     console.log('true');
@@ -9,3 +10,35 @@ isEven(5);
 isEven(2);
 isEven(4);
 isEven(3);
+
+// 2nd problem
+
+// const isCapitalized = function (string) {
+//   const  isCapitalized.map(string)
+// };
+
+// Function Expressionh
+
+const isEven1 = function (number) {
+  if (number % 2) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
+};
+isEven1(2);
+isEven1(4);
+isEven1(3);
+
+// Arrow Function
+
+const isEven3 = number => {
+  if (number % 2) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
+};
+isEven3(2);
+isEven3(4);
+isEven3(3);
